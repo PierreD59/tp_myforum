fdc5afbd8c31582e4a4448349fc485cef027a098
PierreD59 <pierre.dmt59@gmail.com>
2021-03-17 10:21:22 +0100

    added functionality to delete a comment, an article. If an article is deleted, the linked comment is also deleted

--- pages/article.content.php
+++ pages/article.content.php
@@ -39,7 +39,16 @@ if (isset($_POST['commentSubmit'])) {
             <hr>
             <p><?= $data['chapeau']; ?></p>
             <p><?= $data['content']; ?></p>
-            <p>Posté le <?= $data['publiched_date']; ?> par <?= $data['author']; ?></p>
+
+            <div class="row">
+                <div class="col-md-6 d-flex justify-content-start">
+                    <p>Posté le <?= $data['publiched_date']; ?> par <?= $data['author']; ?></p>
+                </div>
+                <div class="col-md-6 d-flex justify-content-end">
+                    <a href="?page=edit&id=<?= $data['id']; ?>"><i class="fas fa-cog p-1"></i></a>
+                    <a href="?page=delete&id=<?= $data['id']; ?>"><i class="fas fa-times p-1"></i></a>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -70,6 +79,10 @@ if (isset($_POST['commentSubmit'])) {
             while (($dataComment = $query->fetch())) { ?>
 
                 <div class="bg-light border m-3 p-3">
+                    <div class="d-flex justify-content-end">
+                        <a href="?page=edit&id=<?= $dataComment['id']; ?>"><i class="fas fa-cog p-1"></i></a>
+                        <a href="?page=delete&id=<?= $dataComment['id']; ?>"><i class="fas fa-times p-1"></i></a>
+                    </div>
                     <p><?= $dataComment['pseudo'] ?></p>
                     <p><?= $dataComment['publiched_date']; ?></p>
                     <p><?= $dataComment['comment']; ?></p>
---
+++ pages/delete.content.php
@@ -0,0 +1,20 @@
+<?php
+$database = require_once dirname(__FILE__) . '/../utils/database.utils.php';
+$id = $_GET['id'];
+
+$deleteComment = $database->prepare("DELETE FROM `comments` WHERE id = :id ");
+$deleteComment->execute([
+    "id" => $id,
+]);
+
+$deleteArticle = $database->prepare("DELETE FROM `articles` WHERE id = :id ");
+$deleteArticle->execute([
+    "id" => $id,
+]);
+
+$deleteCategory = $database->prepare("DELETE FROM `categorys` WHERE id = :id ");
+$deleteCategory->execute([
+    "id" => $id,
+]);
+
+header('Location:' . $_SERVER['HTTP_REFERER']);
