8aab45c1cdc11d37c625eab628a2f340d4670db8
PierreD59 <pierre.dmt59@gmail.com>
2021-03-19 19:30:44 +0100

    Now we can see the total number of messages of the user.

--- pages/profile.content.php
+++ pages/profile.content.php
@@ -5,6 +5,17 @@ $query->execute([
      "id" => $_GET['id'],
      ]);
 $user = $query->fetch();
+
+$count = $database->prepare('SELECT * FROM `articles` WHERE user_id =' . $user['id']);
+$count->execute();
+$countArticle = $count->rowCount();
+
+$count = $database->prepare('SELECT * FROM `comments` WHERE user_id =' . $user['id']);
+$count->execute();
+$countComment = $count->rowCount();
+
+$countMessage = $countArticle + $countComment;
+
 ?>
 
 <div class="home">
@@ -16,17 +27,16 @@ $user = $query->fetch();
     <div class="row p-3 m-0">
         <div class="profileBlock detailProfile p-3 col-md-6 border-end">
             <h2>Information de <?= $user['pseudo']; ?></h2>
+            <hr>
             <ul>
                 <li>Adresse mail : <?php $user['email_adress']; ?></li>
                 <li>Date d'inscription : <?php $user['registration_date']; ?></li>
-                <?php $count = $database->prepare('SELECT * FROM `articles` WHERE user_id =' . $user['id']);
-                    $count->execute();
-                    $countUser = $count->rowCount(); ?>
-                <li>Messages : <?= $countUser; ?></li>              
+                <li>Messages : <?= $countMessage; ?></li>              
             </ul>
         </div>
         <div class="profileBlock imgProfile col-md-4">
             <h2>Photo de Profil</h2>
+            <hr>
             <img src="<?= $user['illustration_image_url']; ?>" alt="Photo profil">
         </div>
 
